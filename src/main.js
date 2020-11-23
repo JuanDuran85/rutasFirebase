2275a38890e772d3e68124b57d9ac2ab0e3e71c6
JuanDuran85 <duran85juan@gmail.com>
2020-11-23 20:20:55 -0300

    agregando firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router';
+import store from './store/store';
+//import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { firebaseConfig } from "./config/firebaseConfig";
 
-Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+firebase.initializeApp(firebaseConfig);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container my-5">
+    <h1>Login de Usuarios</h1>
+    <div class="bg-light">
+      <b-form >
+        <b-form-group id="input-group-1" label="Email address:" label-for="input-1" description="We'll never share your email with anyone else.">
+          <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Enter email"></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+          <b-form-input id="input-2" v-model="form.clave" type="password" required placeholder="Enter password"></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      form: {
+        email: '',
+        clave: ''
+      }
+      
+    }
+  },
+}
+</script>
